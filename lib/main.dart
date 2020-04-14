2dc305f784769986c934df031fa6a64955db5cff
ikaridana <ika.aridana@gmail.com>
2020-04-14 21:40:26 +0800

    Fix Bug minSDKVersion problem

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'widget/voting_app.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       title: "Voting App",
       home: VotingApp(),
     );
   }
-}
-
-class VotingApp extends StatefulWidget {
-  @override
-  _VotingAppState createState () => _VotingAppState();
-}
-
-class _VotingAppState extends State<VotingApp> {
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: new AppBar(
-        title: Text('Let\'s Vote'),
-      ),
-      body: _buildBody(context),
-    );
-  }
-
-  Widget _buildBody(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('baby').snapshots(),
-      builder: (context, snapshot){
-        if(!snapshot.hasData) return LinearProgressIndicator();
-        return _buildList(context, snapshot.data.documents);
-      },
-    );
-  }
-
-  Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot){
-    return ListView(
-      padding: const EdgeInsets.only(top: 10.0),
-      children: snapshot.map((data) => _buildListItem(context, data)).toList(),
-    );
-  }
-
-  Widget _buildListItem(BuildContext context, DocumentSnapshot data){
-    final record = Record.fromSnapshot(data);
-    return Padding(
-      key: ValueKey(record.name),
-      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-      child: Container(
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.blue),
-          borderRadius: BorderRadius.circular(5.0),
-        ),
-        child: ListTile(
-          title: Text(record.name),
-          trailing: Text(record.votes.toString()),
-          onTap: () => record.reference.updateData({'votes':FieldValue.increment(1)}),
-        ),
-      ),
-    );
-  }
-}
-
-class Record {
-  final String name;
-  final int votes;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-    : assert(map['name'] != null),
-      assert(map['votes'] != null),
-      name = map['name'],
-      votes = map['votes'];
-
-  Record.fromSnapshot(DocumentSnapshot snapshot)
-    : this.fromMap(snapshot.data, reference: snapshot.reference);
-
-  @override
-  String toString() => "Record<$name:$votes>";
 }
